c9fdcb0c9722d159c3053d9a2fd37ebe2bca7a8c
MickaelBoixiere <mickael.boixiere@zenika.com>
2021-01-21 14:55:47 +0100

    api(node): add docker file

---
+++ api-nodejs/README.md
@@ -0,0 +1,16 @@
+# Node API
+
+## Docker
+
+Build 
+
+```shell
+docker build -t zgreen/api-node .
+```
+
+Run
+
+```shell
+docker run --name zgree_node_api -p 3031:3031 -d zgreen/api-node
+```
+
--- api-nodejs/server.js
+++ api-nodejs/server.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3031
 
 app.get('/', (req, res) => {
     if (req.query.account === undefined ) {
