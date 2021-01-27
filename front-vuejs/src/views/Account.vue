a5e11b6423323303632a64f09ac14dcf6c37e582
MickaelBoixiere <mickael.boixiere@zenika.com>
2021-01-27 09:42:58 +0100

    fron(vuejs): load data from api and display it

--- front-vuejs/src/views/Account.vue
+++ front-vuejs/src/views/Account.vue
@@ -1,16 +1,22 @@
 <template>
   <div>
     <h1>Account</h1>
-    <button v-on:click="getAccountData">Get account Data</button>
-    <div>{{ accountData }}</div>
-    <div>Balance: {{ accountData.balanace }}</div>
+    <div>{{ accountData.owner }}</div>
+    <div>Balance: {{ accountData.balance }}</div>
+    <h3>Dernières opérations</h3>
+    <ul id="example-2">
+      <li v-for="(operation, index) in accountData.lastoperations">
+        {{ index }} - {{ operation.date }} - {{ operation.type }} -
+        {{ operation.value }} - {{ operation.description }}
+      </li>
+    </ul>
   </div>
 </template>
 <script>
 export default {
   name: "Account",
   props: {
-    msg: String
+    accountNumber: String
   },
   data() {
     return {
@@ -20,18 +26,8 @@ export default {
   created() {
     fetch("http://localhost:3031/?account=001")
       .then(response => response.json())
-      .then(data => console.log(data))
       .then(data => (this.accountData = data))
       .catch(error => console.log(error));
-  },
-  methods: {
-    getAccountData() {
-      fetch("http://localhost:3031/?account=001")
-        // With the fetch API we need to call the json() function which reads the response to the completion.
-        // This also returns a promise, so we need to chain a new then method to get the JSON data that we want.
-        .then(response => response.json())
-        .then(data => (this.accountData = data));
-    }
   }
 };
 </script>
