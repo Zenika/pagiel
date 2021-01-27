d0b55fc88ec8287d92ac343bc170aaaadd4bb716
MickaelBoixiere <mickael.boixiere@zenika.com>
2021-01-27 15:25:49 +0100

    test(vuejs): update e2e tests

--- front-vuejs/tests/e2e/specs/test.js
+++ front-vuejs/tests/e2e/specs/test.js
@@ -1,8 +1,8 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-describe("My First Test", () => {
+describe("Home Test", () => {
   it("Visits the app root url", () => {
     cy.visit("/");
-    cy.contains("h1", "Welcome to Your Vue.js App");
+    cy.contains("h1", "Home");
   });
 });
