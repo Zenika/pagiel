95fa2f8746b91e7b052b1d1ade923e916e5b3ef8
Damien-Ar <damien.aranda42@gmail.com>
2022-06-01 10:50:45 +0200

    remove local node and vue project

--- front-vuejs/tests/e2e/specs/test.js
+++
@@ -1,8 +0,0 @@
-// https://docs.cypress.io/api/introduction/api.html
-
-describe("Home Test", () => {
-  it("Visits the app root url", () => {
-    cy.visit("/");
-    cy.contains("h1", "Home");
-  });
-});
