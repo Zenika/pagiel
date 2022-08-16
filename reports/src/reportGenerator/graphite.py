336d6d27cc707f6f800968d0f03d98fb0cab5217
Damien-Ar <damien.aranda42@gmail.com>
2022-08-16 15:24:17 +0100

    update sitespeed version

--- reports/src/reportGenerator/graphite.py
+++ reports/src/reportGenerator/graphite.py
@@ -6,13 +6,13 @@ urlRegex = "https?:\/\/([\-a-zA-Z0-9.]+(?:\:\d{1,5})?)\/?((?:\/?[a-zA-Z0-9\-_?=&
 
 def extract_url_and_page(config_url):
     result = re.search(urlRegex, config_url)
-    if not result:
+    if not result or not result.group(1):
         raise InvalidUrlException(config_url)
     return (result.group(1).replace('.', '_'), result.group(2).replace('/', '_') if result.group(2) else '_')
 
-def prepare_endpoint(endpoint, test_url, function, grouped):
+def prepare_endpoint(endpoint, test_url, function):
     (db_url, page) = extract_url_and_page(test_url)
-    return endpoint.replace("$groupUrl", db_url + "." if grouped else "").replace("$url", db_url).replace("$page", page).replace("$function", function)
+    return endpoint.replace("$groupUrl", db_url).replace("$url", db_url).replace("$page", page).replace("$function", function)
 
 
 class GraphiteClient:
@@ -20,8 +20,8 @@ class GraphiteClient:
         self.url = url
         self.auth= auth
 
-    def query(self, raw_endpoint, test_url, grouped=True, function='median', tps_from="-30d", tps_to=None, max_data_points=1812, format="json"):
-        endpoint = f'{self.url}/render?target={prepare_endpoint(raw_endpoint, test_url, function, grouped)}&format={format}&from={tps_from}{"&to={tps_to}" if tps_to else ""}&maxDataPoints={max_data_points}'
+    def query(self, raw_endpoint, test_url, function='median', tps_from="-30d", tps_to=None, max_data_points=1812, format="json"):
+        endpoint = f'{self.url}/render?target={prepare_endpoint(raw_endpoint, test_url, function)}&format={format}&from={tps_from}{"&to={tps_to}" if tps_to else ""}&maxDataPoints={max_data_points}'
         results = requests.get(endpoint, auth=self.auth).json()
         for result in results:
             if(result.get("datapoints")):
