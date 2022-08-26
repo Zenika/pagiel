feef8c8181a01f1fd42fd2cebb9828031fcf33e2
Damien-Ar <damien.aranda42@gmail.com>
2022-08-26 09:48:17 +0100

    breakdown process function

--- input/url-file-converter/src/__main__.py
+++ input/url-file-converter/src/__main__.py
@@ -1,4 +1,3 @@
-import sys
 import os
 import yaml
 import shutil
@@ -29,7 +28,7 @@ ROBOT_COOKIE = """
     Click Element   {cookie_btn}
 """
 
-def parse_args():
+def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description = "Convert user url request to required file format from container")
     parser.add_argument("inputFile", help="User input file")
     parser.add_argument("--ecoIndexFile", help="file for eco-index", default="/home/urlconverter/results/urls.yaml")
@@ -38,37 +37,44 @@ def parse_args():
     parser.add_argument("--robotFolder", help="folder for robot tests", default="/home/urlconverter/tests/")
     return parser.parse_args()
 
+def filter_dict_list_by_key(dict_list: list, expected_keys: list) -> list:
+    return [{key: value for key, value in url.items() if key in expected_keys} for url in dict_list]
+
+def save_yaml_file(file_name: str, url_list: list, expected_keys: list):
+    with open(file_name, 'w') as file:
+        yaml.dump(filter_dict_list_by_key(url_list, expected_keys), file)
+
+def save_robot_files(robot_folder: str, url_list: list):
+    robot_resources = [ROBOT_RESOURCE_TEMPLATE.format(url=url["url"], name=url["name"], final_url=url.get("final_url", url["url"])) for url in url_list]
+    robot_tests = []
+    for url in url_list:
+        test = ROBOT_TEST_TEMPLATE.format(name=url["name"], cookie=ROBOT_COOKIE if url.get("cookie_btn") else "")
+        if url.get("cookie_btn"):
+            test = test.format(cookie_btn=url.get("cookie_btn"))
+        robot_tests.append(test)
+
+
+    robot_resource_file_name = os.path.join(robot_folder, "generated-resource.resource")
+    shutil.copyfile(RESOURCE_FILES.format(file="resource.resource"), robot_resource_file_name)
+    with open(robot_resource_file_name, "a") as robot_resource_file:
+        robot_resource_file.write("\n".join(robot_resources))
+    
+    robot_test_file_name = os.path.join(robot_folder, "generated-ping.robot")
+    shutil.copyfile(RESOURCE_FILES.format(file="tests.robot"), robot_test_file_name)
+    with open(robot_test_file_name, "a") as robot_test_file:
+        robot_test_file.write("\n".join(robot_tests))
+
 def process(args):
     with open(args.inputFile) as url_input_file:
         url_list = yaml.load(url_input_file, Loader=yaml.FullLoader)
-
-        with open(args.ecoIndexFile, 'w') as ecoIndexUrlFile:
-            yaml.dump([{k: v for k,v in url.items() if k in GREENIT_INPUT_FILE_ARGS} for url in url_list], ecoIndexUrlFile)
+        
+        save_yaml_file(args.ecoIndexFile, url_list, GREENIT_INPUT_FILE_ARGS)
+        save_yaml_file(args.yellowLabToolsFile, url_list, YELLOWLABTOOLS_INPUT_FILE_ARGS)
 
         with open(args.sitespeedFile, 'w') as sitespeed_urls:
             sitespeed_urls.write("\n".join([url["url"] for url in url_list]))
 
-        with open(args.yellowLabToolsFile, 'w') as yellowlabtoolsUrlFile:
-            yaml.dump([{k: v for k,v in url.items() if k in YELLOWLABTOOLS_INPUT_FILE_ARGS} for url in url_list], yellowlabtoolsUrlFile)
-
-        robot_resources = [ROBOT_RESOURCE_TEMPLATE.format(url=url["url"], name=url["name"], final_url=url.get("final_url", url["url"])) for url in url_list ]
-        robot_tests = []
-        for url in url_list:
-            test = ROBOT_TEST_TEMPLATE.format(name=url["name"], cookie= ROBOT_COOKIE if url.get("cookie_btn") else "")
-            if url.get("cookie_btn"):
-                test = test.format(cookie_btn=url.get("cookie_btn"))
-            robot_tests.append(test)
-
-
-        robot_resource_file_name = os.path.join(args.robotFolder, "generated-resource.resource")
-        shutil.copyfile(RESOURCE_FILES.format(file="resource.resource"), robot_resource_file_name)
-        with open(robot_resource_file_name, "a") as robot_resource_file:
-            robot_resource_file.write("\n".join(robot_resources))
-        
-        robot_test_file_name = os.path.join(args.robotFolder, "generated-ping.robot")
-        shutil.copyfile(RESOURCE_FILES.format(file="tests.robot"), robot_test_file_name)
-        with open(robot_test_file_name, "a") as robot_test_file:
-            robot_test_file.write("\n".join(robot_tests))
+        save_robot_files(args.robotFolder, url_list)
 
 def main():
     args = parse_args()
