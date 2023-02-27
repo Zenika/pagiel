3eb3e83208b4ea4a1cdbde24bd5b558b0bc60808
Damien-Ar <damien.aranda42@gmail.com>
2023-02-27 17:46:17 +0100

    fix edit wrong config file

--- scripts/sitespeed-conf.py
+++ scripts/sitespeed-conf.py
@@ -14,14 +14,14 @@ except ValueError:
 
 try:
     # trying to read the default config file
-    with open('smartwatts/config-default.json', encoding='utf8') as sitespeed_default_config_file:
+    with open('sitespeed/config/config-default.json', encoding='utf8') as sitespeed_default_config_file:
         json_config = json.load(sitespeed_default_config_file)
     # customize the config
-    json_config['output']['pusher_power']['uri'] = f'http://{host}'
-    json_config['output']['pusher_power']['port'] = port
-    json_config['output']['pusher_power']['db'] = bucket
-    json_config['output']['pusher_power']['token'] = token
-    json_config['output']['pusher_power']['org'] = org
+    json_config['influxdb']['host'] = host
+    json_config['influxdb']['port'] = port
+    json_config['influxdb']['database'] = bucket
+    json_config['influxdb']['token'] = token
+    json_config['influxdb']['organisation'] = org
 
     # create or overwrite the config file, no need for test
     with open('sitespeed/config/config.json', 'w', encoding='utf8') as sitespeed_config_file:
