823f3419a369b19e903fd7977effed6eb5541b8d
edouardglx <edouard.gloux@zenika.com>
2023-05-05 11:41:55 +0200

    config frontend

---
+++ Pagiel-front/vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vitest/config'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./setupTest.js",
+  },
+})
