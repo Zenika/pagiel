4469bff24155e9723c532443f674e06b1cf84d26
IssamLynx <hm_bakha@esi.dz>
2023-07-19 17:54:09 +0200

    Added indicators parameters

--- backend/server.js
+++ backend/server.js
@@ -2,17 +2,56 @@ const express = require("express");
 const dotenv = require("dotenv");
 const { execFile } = require("child_process");
 
-//Load env vars
 dotenv.config({ path: "./config/config.env" });
 
-//Route files
 const app = express();
 
-//Body Parser
 app.use(express.json());
 
-app.get("/start", (req, res) => {
-  execFile("bash", ["../pagiel.sh"], (error, stdout, stderr) => {
+app.post("/start", (req, res) => {
+  const options = req.body;
+  let scriptArgs = [];
+
+  if (options.disablePowerAPI) {
+    scriptArgs.push("-P");
+  }
+
+  if (options.disableGreenItAnalysis) {
+    scriptArgs.push("-G");
+  }
+
+  if (options.disableSitespeed) {
+    scriptArgs.push("-S");
+  }
+
+  if (options.disableYellowLabTools) {
+    scriptArgs.push("-Y");
+  }
+
+  if (options.disableReportGeneration) {
+    scriptArgs.push("-R");
+  }
+
+  if (options.disableRobotFramework) {
+    scriptArgs.push("-F");
+  }
+
+  if (options.dockerMode === "image") {
+    scriptArgs.push("-d");
+    scriptArgs.push("--docker-image");
+    scriptArgs.push(options.dockerImage);
+    scriptArgs.push("--docker-port");
+    scriptArgs.push(options.dockerPort || "80");
+    scriptArgs.push("--docker-front-container");
+    scriptArgs.push(options.dockerFrontContainer || "test-container");
+  } else if (options.dockerMode === "compose") {
+    scriptArgs.push("-D");
+    scriptArgs.push("--docker-compose-file");
+    scriptArgs.push(options.dockerComposeFile);
+  }
+  res.status(200).send("Pagiel is launched");
+
+  execFile("bash", ["../pagiel.sh", ...scriptArgs], (error, stdout, stderr) => {
     console.log(`Script output: ${stdout}`);
 
     if (error) {
@@ -21,18 +60,15 @@ app.get("/start", (req, res) => {
         .status(500)
         .send("An error occurred while executing the script");
     }
-
-    res.send("Script execution completed");
   });
 });
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5005;
 const server = app.listen(PORT, () => {
   console.log(`server is running in ${process.env.NODE_ENV} mode on ${PORT}`);
 });
-// Handle unhandled promise rejections
+
 process.on("unhandledRejection", (err, promise) => {
   console.log(`Error', ${err.message}`);
-  //Close Server & exit process
   server.close(() => process.exit(1));
 });
